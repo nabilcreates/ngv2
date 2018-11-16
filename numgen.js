172405835948a43f5dfbbffead28b4adaa156101
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-16 11:04:40 +0800

    .filter() - return number if it matches

--- numgen.js
+++ numgen.js
@@ -37,6 +37,7 @@ function NumGen() {
         // console.log the only ones that match the requirement
         if (number.slice(start, end) == requirement) {
             console.log(`${number}`)
+            return number
         }
 
         // // but if errors is set to true (from the obj argument) then it will console log No Match followed by the number (wont console log if errors is set to false)
@@ -53,11 +54,11 @@ function NumGen() {
 // DEMO:
 // let newgen = new NumGen()
 
-// for (var i = 0; i < 10000000; i++) {
+// for (var i = 0; i < 1000; i++) {
 //     newgen.filter({
 //         number: newgen.generate(8),
 //         start: 0,
-//         requirement: 84680094,
+//         requirement: 84,
 //         errors: false
 //     })
 // }
