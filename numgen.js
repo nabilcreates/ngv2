1a35f0be9bde407a8a5c958a0230e390d322a44d
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-16 10:19:39 +0800

    commenting code...

--- numgen.js
+++ numgen.js
@@ -1,19 +1,27 @@
 function NumGen() {
+
+    // generate numbers
     this.generate = function (length) {
 
+        // this.length is the length argument
         this.length = length;
 
+        // number is an empty string (so it can concat with numbers later on)
         let number = ''
 
+        // so for each iteration, it will concat to the number
         for (var n = 0; n < this.length; n++) {
             number += Math.floor(Math.random() * 10)
         }
 
+        // return the number (can be used with filter)
         return number
     }
 
+    // filter only stuff that matches the requirements set by the user
     this.filter = function (obj) {
 
+        // takes obj as an argument, destructuring the object
         let {
             number,
             start,
@@ -24,10 +32,12 @@ function NumGen() {
 
         // console.log(number.slice(start, end) == requirement)
 
+        // console.log the only ones that match the requirement
         if (number.slice(start, end) == requirement) {
             console.log(`${number}`)
         }
 
+        // but if errors is set to true (from the obj argument) then it will console log No Match followed by the number (wont console log if errors is set to false)
         if(errors){
             if (number.slice(start, end) !== requirement) {
                 console.log(`No Match (${number})`)
